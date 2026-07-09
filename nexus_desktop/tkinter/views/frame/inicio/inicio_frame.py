806ae6b2fa3fdcbdf5f65994c1acef4f2e4ef2ba
Julian Lara <Julian.Lara@axity.com>
2026-07-09 17:33:29 -0600

    ajustes titulo

--- nexus_desktop/tkinter/views/frame/inicio/inicio_frame.py
+++ nexus_desktop/tkinter/views/frame/inicio/inicio_frame.py
@@ -1,16 +1,20 @@
 import ttkbootstrap as tb
 import tkinter as tk
 import locale
-from nexus_desktop.tkinter.views.component.label.label_tittle_widget import LabelTittleWidget
-from nexus_desktop.tkinter.services.multi_lenguage.multi_lenguage_service import MultiLanguageService
+from nexus_desktop.tkinter.views.component.label.label_tittle_widget import (
+    LabelTittleWidget,
+)
+from nexus_desktop.tkinter.services.multi_lenguage.multi_lenguage_service import (
+    MultiLanguageService,
+)
 
-class InicioFrame():
+
+class InicioFrame:
     def __init__(self, contenedor_principal, language: str):
         self.contenedor_principal = contenedor_principal
-        self.lenguage = MultiLanguageService(language= language).load_transaction()
+        self.lenguage = MultiLanguageService(language=language).load_transaction()
 
     def crear_frame_inicio(self) -> tb.Frame:
         p_inicio = tb.Frame(self.contenedor_principal)
-        LabelTittleWidget(tbframe= p_inicio, text=self.lenguage["welcome"]).grid(row=0, column= 0)
+        LabelTittleWidget(tbframe=p_inicio, text=self.lenguage["welcome"]).pack()
         return p_inicio
-    
